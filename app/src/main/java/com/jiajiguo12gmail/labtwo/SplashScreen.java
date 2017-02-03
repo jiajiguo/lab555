9ec41d8e46ab0d555fd8d2e2f66de7b49392c021
jiajiguo <jiajiguo12@gmail.com>
2017-02-03 01:46:16 -0500

    Initial commit

---
+++ app/src/main/java/com/jiajiguo12gmail/labtwo/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.jiajiguo12gmail.labtwo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+/**
+ * Created by Jiaji Guo on 2017-02-02.
+ */
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), secondMainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
---
+++ app/src/main/java/com/jiajiguo12gmail/labtwo/secondMainActivity.java
@@ -0,0 +1,14 @@
+package com.jiajiguo12gmail.labtwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class secondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_main);
+    }
+}
