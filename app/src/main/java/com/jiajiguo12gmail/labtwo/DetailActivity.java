0dd699bc6bee1ed707e6b5116a00c1dd9c9b3136
jiajiguo <jiajiguo12@gmail.com>
2017-02-18 21:49:02 -0500

    feb 18 21:48

--- app/src/main/java/com/jiajiguo12gmail/labtwo/DetailActivity.java
+++ app/src/main/java/com/jiajiguo12gmail/labtwo/DetailActivity.java
@@ -21,7 +21,7 @@ import android.widget.TextView;
  * Created by Jiaji Guo on 2017/2/15.
  */
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends AppCompatActivity {
 
 
     @Override
@@ -31,6 +31,12 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.activity_detail);
         TextView detailText1 = (TextView) findViewById(R.id.detailText1);
         TextView stadiumLocation = (TextView) findViewById(R.id.stadiumLocation);
+        TextView opponentTeam = (TextView) findViewById(R.id.opponentTeam);
+        TextView opponentRecord = (TextView) findViewById(R.id.opponentRecord);
+        TextView irishRecord = (TextView) findViewById(R.id.irishRecord);
+        TextView gameScore = (TextView) findViewById(R.id.gameScore);
+        TextView gameStatus = (TextView) findViewById(R.id.gameStatus);
+
         ImageView detailImage1 = (ImageView) findViewById(R.id.detailImage1);
         ImageView detailImage2 = (ImageView) findViewById(R.id.detailImage2);
 
@@ -62,6 +68,14 @@ public class DetailActivity extends Activity {
 
         String[] stringInfo = getIntent().getStringArrayExtra("team");
         detailText1.setText(stringInfo[3]);
+        stadiumLocation.setText(stringInfo[4]);
+        opponentTeam.setText(stringInfo[5]);
+        opponentRecord.setText(stringInfo[6]);
+        irishRecord.setText(stringInfo[7]);
+        gameScore.setText(stringInfo[8]);
+        gameStatus.setText(stringInfo[9]);
+
+
         String picname = stringInfo[0];
         int resID = getResources().getIdentifier(picname,"drawable",getPackageName());
 
--- app/src/main/java/com/jiajiguo12gmail/labtwo/secondMainActivity.java
+++ app/src/main/java/com/jiajiguo12gmail/labtwo/secondMainActivity.java
@@ -15,7 +15,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 
-public class secondMainActivity extends Activity {
+public class secondMainActivity extends AppCompatActivity {
 
 
 
@@ -32,9 +32,16 @@ public class secondMainActivity extends Activity {
 
 
         final ArrayList<String[]> information = new ArrayList();
-        information.add(new String[] {"floridastatelogo","Florida State", "Feb 11","Saturday，February 11th, 18:00"});
-        information.add(new String[] {"bostoncollegelogo","Bostoncollege", "Feb 14","Tuesday, February 14th, 19:00"});
-
+        information.add(new String[] {"northcarolinalogo","North Carolina", "Feb 5","Sunday, February 5, 13:00","Purcell Pavilion at the Joyce Center, Notre Dame, Indiana","Demon Deacons","(14-10)","(18-7)","81-88","Final"});
+        information.add(new String[] {"wakeforestlogo","Wake Forest", "Feb 7","Tuesday, February 7, 19:00","Greensboro Coliseum, Greensboro, North Carolina","Tar Heels","(21-4)","(17-7)","83-76","Final"});
+        information.add(new String[] {"floridastatelogo","Florida State", "Feb 11","Saturday，February 11th, 18:00","Purcell Pavilion at the Joyce Center, Notre Dame, Indiana","Seminoles","(21-5)","(19-7)","72-84","Final"});
+        information.add(new String[] {"bostoncollegelogo","Bostoncollege", "Feb 14","Tuesday, February 14th, 19:00","Silvio O. Conte Forum, Chestnut Hill, Massachusetts","Eagles","(9-18)","(20-7)","76-84","Final"});
+        information.add(new String[] {"northcarolinastatelogo","North Carolina State", "Feb 18","Saturday, February 18, 12:00","PNC Arena, Raleigh, North Carolina","Wolfpack","(14-14)","(21-7)","72-81","Final"});
+        information.add(new String[] {"georgiatechlogo","Georgia Tech", "Feb 26","Sunday, February 26, 18:30 on ESPU","Purcell Pavilion at the Joyce Center, Notre Dame, Indiana","Yellow Jackets","(15-11)","(21-7)","0-0","Not Start yet"});
+        information.add(new String[] {"bostoncollegelogo","Boston College", "March 1","Wednesday, March 1, 20:00 on ESFC","Purcell Pavilion at the Joyce Center, Notre Dame, Indiana","Eagles","(9-18)","(21-7)","0-0","Not Start yet"});
+        information.add(new String[] {"louisvillelogo","Louisville", "March 4","Saturday, March 4, 14:00 on CBS","KFC Yum! Center, Louisville, Kentucky","Cardinals","(22-5)","(21-7)","0-0","Not Start yet"});
+//        information.add(new String[] {"medallogo","ACC Tournament", "March 7"});
+//        information.add(new String[] {"medallogo","ACC Tournament", "March 16"});
         ScheduleAdapter scheduleAdapter = new ScheduleAdapter(this, information);
         final ListView scheduleListView = (ListView) findViewById(R.id.scheduleListView);
         scheduleListView.setAdapter(scheduleAdapter);
